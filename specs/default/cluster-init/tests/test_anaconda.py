532e2515b8635379f6a28321b34ce823c44a0f1f
Ben Watrous <ben.watrous@microsoft.com>
2018-08-07 11:03:58 -0700

    Use project attribute to get anaconda version vs hard-coded path

--- specs/default/cluster-init/tests/test_anaconda.py
+++ specs/default/cluster-init/tests/test_anaconda.py
@@ -3,8 +3,13 @@
 import unittest
 import os
 import jetpack.util
+import jetpack.config
+
+ANACONDA_VERSION = jetpack.config.get("anaconda.version").strip()
+
 
 class TestMaster(unittest.TestCase):
+    
     def test_basic(self):
-        anaconda_license = "/opt/anaconda/4.3.0/LICENSE.txt"
+        anaconda_license = "/opt/anaconda/%s/LICENSE.txt" % ANACONDA_VERSION
         self.assertTrue(os.path.isfile(anaconda_license))
