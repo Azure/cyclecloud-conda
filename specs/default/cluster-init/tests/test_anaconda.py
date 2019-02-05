1a49152db423c205d9ec32a263e298963c75e8b4
Dan Harris <dan.harris@microsoft.com>
2019-02-05 09:53:09 -0500

    Add better automated tests

--- specs/default/cluster-init/tests/test_anaconda.py
+++ specs/default/cluster-init/tests/test_anaconda.py
@@ -1,15 +1,27 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+import subprocess
 import unittest
-import os
-import jetpack.util
+
 import jetpack.config
 
 ANACONDA_VERSION = jetpack.config.get("anaconda.version").strip()
 
+class TestAnaconda(unittest.TestCase):
+    def test_install(self):
+        """ Verifies conda was installed """
+        self.assertEquals("/opt/anaconda/%s/bin/conda" % ANACONDA_VERSION, subprocess.check_output("which conda", shell=True).strip())
 
-class TestMaster(unittest.TestCase):
-    
     def test_basic(self):
-        anaconda_license = "/opt/anaconda/%s/LICENSE.txt" % ANACONDA_VERSION
-        self.assertTrue(os.path.isfile(anaconda_license))
+        """ Create a test-env with numpy installed and verify functionality """
+        script = """
+#!/bin/bash
+conda create -c conda-forge -n test-env python=2.7 numpy --yes
+source activate test-env
+python -c "import numpy; assert(sum(numpy.array([1,2,3,4]) + 1) == 14)"
+source deactivate
+conda remove --name myenv --all
+        """
+        with open("/tmp/test.sh", 'w') as f:
+            f.write(script)
+        subprocess.check_call(["sh /tmp/test.sh"], shell=True)
